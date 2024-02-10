6e21f34314b76f86d67c898cd0517408ef51fbe4
Baptiste <baptiste.hardy@toulouse-inp.fr>
2024-02-10 22:37:29 +0100

    Add __init__.py file for package usage

---
+++ filtered_drag/__init__.py
@@ -0,0 +1,4 @@
+# This file is part of the fTFM_ANN_modeling project (https://github.com/bahardy/fTFM_ANN_modeling.git) distributed under BSD-3-Clause license 
+# Copyright (c) Baptiste HARDY. All rights reserved. 
+
+from .terminalVelocity import getTerminalVelocity
